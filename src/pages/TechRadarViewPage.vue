b34a4fde8e20e8e594e9f23a8982c9e63196c22d
Caio Kinzel Filho <caiokf@gmail.com>
2025-12-09 18:28:20 +1000

    refactor(radar): extract SpotlightLoader into reusable component

--- src/pages/TechRadarViewPage.vue
+++ src/pages/TechRadarViewPage.vue
@@ -75,12 +75,7 @@
     </div>
 
     <div v-else class="loading">
-      <div class="spotlight-container">
-        <div class="spotlight spotlight-ne"></div>
-        <div class="spotlight spotlight-nw"></div>
-        <div class="spotlight spotlight-sw"></div>
-        <div class="spotlight spotlight-se"></div>
-      </div>
+      <SpotlightLoader :size="180" />
     </div>
   </div>
 </template>
@@ -94,6 +89,7 @@
   import RadarLegend from "../components/radar/RadarLegend.vue";
   import Search from "../components/radar/Search.vue";
   import RadarHeader from "../components/radar/RadarHeader.vue";
+  import SpotlightLoader from "../components/SpotlightLoader.vue";
   import { Radar } from "../models/radar";
   import { SampleDataProvider } from "../data/providers/sample-data-provider";
   import { GoogleSheetsProvider } from "../data/providers/google-sheets-provider";
@@ -125,7 +121,6 @@
   const MIN_LOADING_DURATION = 1000;
 
   const radar = shallowRef<Radar | null>(null);
-  const techRadarRef = ref<InstanceType<typeof TechRadar> | null>(null);
   const selectedQuadrant = ref<QuadrantPosition | null>(null);
   const hoveredBlipId = ref<number | null>(null);
   const expandedBlipId = ref<number | null>(null);
@@ -331,165 +326,6 @@
     align-items: center;
     justify-content: center;
     min-height: calc(100vh - 112px);
-  }
-
-  .spotlight-container {
-    position: relative;
-    width: 500px;
-    height: 500px;
-  }
-
-  .spotlight {
-    position: absolute;
-    width: 180px;
-    height: 180px;
-    border-radius: 50%;
-    filter: blur(60px);
-    opacity: 1;
-  }
-
-  .spotlight-ne {
-    background-color: var(--quadrant-NE);
-    animation: dance-ne 8s linear infinite;
-  }
-
-  .spotlight-nw {
-    background-color: var(--quadrant-NW);
-    animation: dance-nw 15s linear infinite;
-  }
-
-  .spotlight-sw {
-    background-color: var(--quadrant-SW);
-    animation: dance-sw 13s linear infinite;
-  }
-
-  .spotlight-se {
-    background-color: var(--quadrant-SE);
-    animation: dance-se 14s linear infinite;
-  }
-
-  /* NE: smooth orbit around top-right area */
-  @keyframes dance-ne {
-    0% {
-      transform: translate(280px, 40px) scale(1);
-    }
-    12.5% {
-      transform: translate(310px, 70px) scale(1.05);
-    }
-    25% {
-      transform: translate(300px, 110px) scale(1.08);
-    }
-    37.5% {
-      transform: translate(260px, 130px) scale(1.05);
-    }
-    50% {
-      transform: translate(220px, 110px) scale(1);
-    }
-    62.5% {
-      transform: translate(200px, 70px) scale(0.95);
-    }
-    75% {
-      transform: translate(220px, 30px) scale(0.92);
-    }
-    87.5% {
-      transform: translate(260px, 20px) scale(0.95);
-    }
-    100% {
-      transform: translate(280px, 40px) scale(1);
-    }
-  }
-
-  /* NW: smooth orbit around top-left area */
-  @keyframes dance-nw {
-    0% {
-      transform: translate(60px, 60px) scale(1.05);
-    }
-    12.5% {
-      transform: translate(30px, 90px) scale(1);
-    }
-    25% {
-      transform: translate(20px, 130px) scale(0.95);
-    }
-    37.5% {
-      transform: translate(40px, 160px) scale(0.92);
-    }
-    50% {
-      transform: translate(80px, 150px) scale(0.95);
-    }
-    62.5% {
-      transform: translate(110px, 120px) scale(1);
-    }
-    75% {
-      transform: translate(100px, 80px) scale(1.05);
-    }
-    87.5% {
-      transform: translate(70px, 50px) scale(1.08);
-    }
-    100% {
-      transform: translate(60px, 60px) scale(1.05);
-    }
-  }
-
-  /* SW: smooth orbit around bottom-left area */
-  @keyframes dance-sw {
-    0% {
-      transform: translate(50px, 280px) scale(0.95);
-    }
-    12.5% {
-      transform: translate(30px, 250px) scale(1);
-    }
-    25% {
-      transform: translate(40px, 210px) scale(1.05);
-    }
-    37.5% {
-      transform: translate(70px, 190px) scale(1.08);
-    }
-    50% {
-      transform: translate(110px, 200px) scale(1.05);
-    }
-    62.5% {
-      transform: translate(130px, 230px) scale(1);
-    }
-    75% {
-      transform: translate(120px, 270px) scale(0.95);
-    }
-    87.5% {
-      transform: translate(80px, 290px) scale(0.92);
-    }
-    100% {
-      transform: translate(50px, 280px) scale(0.95);
-    }
-  }
-
-  /* SE: smooth orbit around bottom-right area */
-  @keyframes dance-se {
-    0% {
-      transform: translate(290px, 270px) scale(1.08);
-    }
-    12.5% {
-      transform: translate(260px, 290px) scale(1.05);
-    }
-    25% {
-      transform: translate(220px, 280px) scale(1);
-    }
-    37.5% {
-      transform: translate(200px, 250px) scale(0.95);
-    }
-    50% {
-      transform: translate(210px, 210px) scale(0.92);
-    }
-    62.5% {
-      transform: translate(240px, 190px) scale(0.95);
-    }
-    75% {
-      transform: translate(280px, 200px) scale(1);
-    }
-    87.5% {
-      transform: translate(310px, 230px) scale(1.05);
-    }
-    100% {
-      transform: translate(290px, 270px) scale(1.08);
-    }
   }
 
   /* Mobile Quadrant Grid - hidden by default, shown below 1000px */
